fe53a6a3f3cd65d4c5269a1033b66240dfa6b44e
Octacilio C. Almeida <octa.oca44@gmail.com>
2023-04-09 19:58:17 -0300

    criação do projeto e configuração de libs

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import * as Font from 'expo-font';
+
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    
+    <View style={styles.container}>
+
+      <Text>Ola</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+
+
+
+
